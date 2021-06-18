e1821d968efa98f26cb4c7ec6741105e273f9ec8
gunjan7paliwal <gunjanpaliwal7@gmail.com>
2021-06-18 17:12:58 +0530

    code cleanup

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StarComponent } from './star.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    StarComponent
-  ],
   imports: [
     CommonModule
   ],
   exports: [
     CommonModule,
-    FormsModule,
-    StarComponent
+    FormsModule
   ]
 })
 export class SharedModule { }
