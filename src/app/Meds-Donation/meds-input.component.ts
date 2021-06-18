4c83497e0d05478e57ad4b0c99215187adb9ef1c
ApoorvaAmbesange <apoorvaambesange@gmail.com>
2021-06-18 20:30:00 +0530

    ädded alert on successful submission

--- src/app/Meds-Donation/meds-input.component.ts
+++ src/app/Meds-Donation/meds-input.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
 
+function hello() {
+  alert('Data Saved!!!');
+}
+
 @Component({
   templateUrl: './meds-input.component.html'
 })
 export class MedsInputComponent {
   public pageTitle = 'Medicine Donation Form';
+
+  
+  save(){
+     hello()
+   }
+ 
 }
